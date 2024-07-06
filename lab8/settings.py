710cc5ed3645399e0b9424d532b4a37ee6642a80
mdrecaldec <mdrecaldec@puce.edu.ec>
2024-07-06 10:09:51 -0500

    Laboratorio Terminado

--- lab8/settings.py
+++ lab8/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'lab8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', #crea un archivo de texto en plano
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+#    'default':{ no puede haber dos default
+#       'ENGINE': 'django.db.backends.postgresql', #crea un archivo de texto en plano
+#        'NAME':'pokedex', #aqui el nombre de la base de datos
+#        'HOST':'localhost',
+#        'PORT':'5432',#puerto
+#        'USER':'postgres',
+#        'PASSWORD':'postgres'
     }
-}
+#}
 
 
 # Password validation
